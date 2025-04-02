3ce28457048d40b29eee414c5971736d462fe711
Tadeo M. Riveros Koloszwa <tadeoriverosk@gmail.com>
2025-04-02 12:40:31 -0300

    Chapter 1 Section 1.3 complete

--- monkey/lexer/lexer_test.go
+++ monkey/lexer/lexer_test.go
@@ -30,6 +30,7 @@ let result = add(five, ten);
 		{token.SEMICOLON, ";"},
 		{token.LET, "let"},
 		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
 		{token.FUNCTION, "fn"},
 		{token.LPAREN, "("},
 		{token.IDENT, "x"},
